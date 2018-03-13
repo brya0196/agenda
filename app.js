1e0ce1fba56cb80569a3c3025f928a26246436dc
MOTORCREDITO\bastacio <bastacio@motorcredito.com.do>
2018-03-13 17:02:29 -0400

    Added the pug view engine to the app, settiup on app.js

--- app.js
+++ app.js
@@ -4,15 +4,22 @@ const bodyParser = require('body-parser')
 // archivo de configuracion
 const config = require('./app.config')
 
-const app = express()
+const app = express() //instancieted the app
 
+// llamando el middleware BodyParser
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+// cargando el pug view
+app.set('views', './views')
+app.set('view engine', 'pug')
+
+//primera peticion al app
 app.get('/', function(req, res) {
-    res.send('Hello World')
+    res.render('index', { msg: 'Eoo' })
 })
 
+//donde se abrira la aplicacion
 app.listen(config.appPort, function() {
     console.log('servidor en el puerto ' + config.appPort)
 })
