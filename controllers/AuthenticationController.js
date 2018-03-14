533492b60b47d03ced0d04744ce2672de05f92b9
MOTORCREDITO\bastacio <bastacio@motorcredito.com.do>
2018-03-14 16:33:37 -0400

    agregado el login de la aplicacion

--- controllers/AuthenticationController.js
+++ controllers/AuthenticationController.js
@@ -2,13 +2,15 @@ const user = require('./../models/user')
 
 function login (req, res) {
 
-    user.findAll()
-        .then(users => {
-            res.send({ users: users })
-        })
-        .catch(err => {
-            res.send({ users: 'No users found' })
-        })
+    res.render('authentication/login' , { })
+
+    // user.findAll()
+    //     .then(users => {
+    //         res.render('authentication/login',{ users: users })
+    //     })
+    //     .catch(err => {
+    //         res.send({ users: 'No users found' })
+    //     })
 
 
 }
