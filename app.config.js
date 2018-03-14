19d0537c1112d941adc1355ea15f01e81b06fee8
MOTORCREDITO\bastacio <bastacio@motorcredito.com.do>
2018-03-14 12:07:35 -0400

    agregada la conxion a la base de datos con sequelize a mysql

--- app.config.js
+++ app.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-    port: process.env.PORT || 3000
+    //app port
+    port: process.env.PORT || 3000,
+
+    //db port
+    dbname: 'agenda',
+    dbusername: 'root',
+    dbpassword: '',
+    dbhost: 'localhost',
+    dbport: '',
+    db: 'mysql'
 }
--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const app = require('./app')
+const Sequelize = require('sequelize')
 
 // archivo de configuracion
 const config = require('./app.config')
 
-//donde se abrira la aplicacion
-app.listen(config.port, function() {
-    console.log('servidor en el puerto ' + config.port)
+const sequelize = new Sequelize(config.dbname, config.dbusername, config.dbpassword, {
+    host: config.dbhost,
+    dialect: config.db
 })
+
+//verificamos que alla conecion a la base de datos
+sequelize.authenticate()
+    .then(() => app.listen(config.port, () => console.log('servidor en el puerto ' + config.port)))
+    .catch(err => console.log(`Hubo un error en la base de datos ${err}`))
